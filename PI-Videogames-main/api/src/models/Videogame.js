071aa984057c6610c1e59dcd25b6fd293e819b13
Aldana-1985 <aldana25_85@hotmail.com>
2023-03-14 22:36:27 -0300

    database - PI

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
-  });
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: 'No description',
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      defaultValue: ['PC'],
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: DataTypes.STRING,
+      defaultValue: '2000-01-01',
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      defaultValue: 0.25,
+    },
+  },{timestamps : false});
 };
